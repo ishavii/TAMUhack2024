7d9f5720b2e005e8b38f84f344b6fb2efa3e3db0
Oshruti <shoruganti@gmail.com>
2024-01-27 20:27:55 -0600

    new fonts

---
+++ science/science.js
@@ -0,0 +1,36 @@
+var elems = [
+    {
+        "img": "barium.jpg",
+        "ele": "Barium"
+    },
+
+    {
+        "img": "calcium.jpg",
+        "ele": "Calcium"
+    },
+
+    {
+        "img": "copper.jpg",
+        "ele": "Copper"
+    },
+    {
+        "img": "lithium.jpg",
+        "ele": "Lithium"
+    },
+
+    {
+        "img": "Pottasium.jpg",
+        "ele": "Potassium"
+    },
+
+    {
+        "img": "rubidum.jpg",
+        "ele": "Rubidium"
+    },
+
+    {
+        "img": "sodium.jpg",
+        "ele": "Sodium"
+    }
+
+]
